a42f65adf211846cbf6f62040a9711c3037170e4
BrandonN <brandonlnyman@gmail.com>
2024-06-10 18:47:53 +0100

    Displays minutes

--- simplestopwatch/src/App.js
+++ simplestopwatch/src/App.js
@@ -3,31 +3,51 @@ import React from 'react';
 
 function App() {
   const [isOn, setIsOn] = React.useState(false);
-  const [timer, setTimer] = React.useState(0);
+  const [startTime, setStartTime] = React.useState(0);
+  const [elapsedTime, setElapsedTime] = React.useState(0);
   
   React.useEffect(() => {
     let interval;
 
     if (isOn) { 
+      setStartTime(Date.now() - elapsedTime);
       interval = setInterval(
-      () => setTimer(timer + 1),
-      1000,
+        () => setElapsedTime(Date.now() - startTime),
+        1,
       );
+    } else {
+      clearInterval(interval);
     }
 
     return () => clearInterval(interval);
-  }, [isOn, timer]);
-
+  }, [isOn, elapsedTime, startTime]);
 
   const onReset = () => {
     setIsOn(false);
-    setTimer(0);
+    setElapsedTime(0);
   };
+
+  const formatTime = () => {
+    const timer = elapsedTime;
+    const getMinutes = Math.floor((timer / (1000 * 60)) % 60);
+    const getSeconds = Math.floor((timer / 1000) % 60);
+    const getMilliseconds = Math.floor(timer % 1000);
+  
+    // Pad the minutes, seconds and milliseconds with zeros on the left if necessary
+    const paddedMinutes = String(getMinutes).padStart(2, '0');
+    const paddedSeconds = String(getSeconds).padStart(2, '0');
+    const paddedMilliseconds = String(getMilliseconds).padStart(3, '0');
+  
+    // Slice the string to get only the first two digits
+    const twoDecimalMilliseconds = paddedMilliseconds.slice(0, 2);
+  
+    return `${paddedMinutes}:${paddedSeconds}:${twoDecimalMilliseconds}`;
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-      <div className="timer">{timer}</div>
+      <div className="timer">{formatTime()}</div>
 
         {!isOn && (
         <button type="button" onClick={() => setIsOn(true)} className="button">
@@ -41,7 +61,7 @@ function App() {
         </button>
       )} 
 
-      <button type="button" disabled={timer === 0} onClick={onReset} className="button">
+      <button type="button" disabled={elapsedTime === 0} onClick={onReset} className="button">
         Reset
       </button>
 
