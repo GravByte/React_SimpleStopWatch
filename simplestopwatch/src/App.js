0b92e105df0e81acd4e5a152f1cce3db25ca473f
BrandonN <brandonlnyman@gmail.com>
2024-06-11 10:57:47 +0100

    Added input box to end timer at specified minutes passed.
    Input text box to set minutes from 1-60 for the timer to forcibly end.
    
    It then plays a sound effect when the timer ends automatically.
    
    Added comments throughout code.

--- simplestopwatch/src/App.js
+++ simplestopwatch/src/App.js
@@ -2,10 +2,26 @@ import './App.css';
 import React from 'react';
 
 function App() {
+  // Variables
   const [isOn, setIsOn] = React.useState(false);
   const [startTime, setStartTime] = React.useState(0);
   const [elapsedTime, setElapsedTime] = React.useState(0);
+  const [userInput, setUserInput] = React.useState();
+
+  // Timer user input limit
+  const handleInputChange = (event) => {
+    setUserInput(parseInt(event.target.value, 10));
+  };
+
+  // Logic for resetting the stopwatch
+  const handleStopwatchReset = () => {
+    const audio = new Audio('./beep.mp3');
+    audio.play();
+    onReset();
+    return '00:00.00';
+  };
   
+  // Stopwatch timer logic
   React.useEffect(() => {
     let interval;
 
@@ -22,11 +38,13 @@ function App() {
     return () => clearInterval(interval);
   }, [isOn, elapsedTime, startTime]);
 
+  // Reset the stopwatch when called
   const onReset = () => {
     setIsOn(false);
     setElapsedTime(0);
   };
 
+  // Format the time to display on the stopwatch like 00:00.00 (mm:ss.ms)
   const formatTime = () => {
     const timer = elapsedTime;
     const getMinutes = Math.floor((timer / (1000 * 60)) % 60);
@@ -35,10 +53,14 @@ function App() {
 
     // If the timer hits 60 minutes, reset the stopwatch
     if (getMinutes >= 60) {
-      onReset();
-      return '00:00.00';
+      return handleStopwatchReset();
     }
-  
+    
+    // If the user enters a time, stop the stopwatch when the time is reached
+    if (userInput > 0 && getMinutes >= userInput) {
+      return handleStopwatchReset();
+    }
+
     // Pad the minutes, seconds and milliseconds with zeros on the left if necessary
     const paddedMinutes = String(getMinutes).padStart(2, '0');
     const paddedSeconds = String(getSeconds).padStart(2, '0');
@@ -47,6 +69,7 @@ function App() {
     // Slice the string to get only the first two digits
     const twoDecimalMilliseconds = paddedMilliseconds.slice(0, 2);
   
+    // Return the formatted time to display on the stopwatch
     return `${paddedMinutes}:${paddedSeconds}.${twoDecimalMilliseconds}`;
   }
 
@@ -77,6 +100,18 @@ function App() {
       <button type="button" disabled={elapsedTime === 0} onClick={onReset} className="button">
         Reset
       </button>
+
+      <p>Set an end time in minutes:</p>
+
+      <input 
+        type="number" 
+        min="0"
+        max="60"
+        value={userInput} 
+        onChange={handleInputChange} 
+        placeholder="Mins"
+        className="input-box"
+      />
 
       </header>
     </div>
